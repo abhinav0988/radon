bf7da1325864e4a598efa0f23a5310028ebab0dd
ABHINAVMANGAL <abhinavmangal68@gmail.com>
2022-06-03 09:15:19 +0530

    demo1

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,19 +1,36 @@
 const express = require('express');
-const logger = require("../logger/logger");
-const helper = require("./../util/helper");
-const formatter = require('./../validator/formatter');
+
+
+
+const lodash = require("lodash")
 
 const router = express.Router();
 
-router.get('/test-me', function (req, res) {
-    logger.welcome();
-    
-    helper.date();
-    helper.month();
-    helper.info();
-    formatter.trimfun()
+router.get('/hello', function (req, res) {
+    let arr1 =['January',"February","March","april","may","june","july","August","September"," October","november","December"];
+    console.log(lodash.chunk(arr1,4))
+    let arr2=[1,3,5,7,9,11,13,15,17,19]
+    console.log(lodash.tail(arr2))
+    let pair =[
+        ["horror","The Shining"],
+        ["drama","Titanic"],
+        ["thriller","Shutter Island"],
+        ["fantasy","Pans Labyrinth"]
 
-    res.send('My first ever api!')
+    ]
+    console.log(lodash.fromPairs(pair))
+    let uni = lodash.union(
+        [2,3,5,6,7],
+        [4,5,8,9,10],
+        [8,11,12,5,10,13],
+        [15,16,17,18,10],
+        [20,21,22,10,8]
+    );
+    console.log(uni)
+   
+   
+
+    res.send('this is your api code!')
 });
 
 
