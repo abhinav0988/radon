e4b9e84a64a299dce665a9b4a3cf595063f53fb8
ABHINAVMANGAL <abhinavmangal68@gmail.com>
2022-06-11 00:17:34 +0530

    demo1

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const route = require('./routes/route.js');
 const { default: mongoose } = require('mongoose');
+const { request } = require('express');
 const app = express();
 
 app.use(bodyParser.json());
@@ -11,15 +12,31 @@ app.use(bodyParser.urlencoded({ extended: true }));
 mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
-.then( () => console.log("MongoDb is connected"))
-.catch ( err => console.log(err) )
+    .then(() => console.log("MongoDb is connected"))
+    .catch(err => console.log(err))
 
-app.use (
-    function (req, res, next) {
-        console.log ("inside GLOBAL MW");
-        next();
-  }
-  );
+
+const address = require("address")
+
+
+app.use("/createUser", (req, res, next) => {
+    var date_ob = new Date();
+    var day = ("0" + date_ob.getDate()).slice(-2);
+    var month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
+    var year = date_ob.getFullYear();
+    var hours = date_ob.getHours();
+    var minutes = date_ob.getMinutes();
+    var seconds = date_ob.getSeconds();
+
+    var dateTime =  day + ":" + month + ":" +year + " :" + hours + ":" + minutes + ":" + seconds;
+
+
+
+    console.log(dateTime, address.ip(), req.method, req.ip);
+
+    next();
+}
+);
 
 app.use('/', route);
 
